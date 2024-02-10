6178b7854e583a16e7e3f9b627f2476028a612f6
HarikrishnanVK <54284966+Harikrishnanvkph@users.noreply.github.com>
2024-02-10 12:07:30 +0530

    Update Card.jsx

--- src/Card.jsx
+++ src/Card.jsx
@@ -9,14 +9,9 @@ export default function Card({tag, title, map}) {
         <p className='title'>{title}</p>
         <hr/>
         <div className='mapped'>
-            <CardBody name={mp[0][0]} change={mp[0][1]}/>
-            <CardBody name={mp[1][0]} change={mp[1][1]}/>
-            <CardBody name={mp[2][0]} change={mp[2][1]}/>
-            <CardBody name={mp[3][0]} change={mp[3][1]}/>
-            <CardBody name={mp[4][0]} change={mp[4][1]}/>
-            <CardBody name={mp[5][0]} change={mp[5][1]}/>
-            <CardBody name={mp[6][0]} change={mp[6][1]}/>
-            <CardBody name={mp[7][0]} change={mp[7][1]}/>
+          {mp.map((a)=>(
+          <CardBody name={a[0]} change={a[1]} />
+          ))}
         </div>
         <button value={tag} className='button'>Button</button>
       </div>
